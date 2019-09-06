53b4bed56b6837c3bb570c69579026c3129caa65
mcangun <mehmetcangun14@gmail.com>
2019-09-06 23:57:27 +0300

    Add files via upload

---
+++ topology.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+typedef long long int lint;
+
+#define MOD 1e9+7
+
+using namespace std;
+
+#define pb push_back
+
+int main(){
+
+  int n, m;
+  scanf("%d %d", &n, &m);
+  while(m--){
+    int xi, yi;
+    scanf("%d %d", &xi, &yi);
+  }
+
+  return 0;
+}
